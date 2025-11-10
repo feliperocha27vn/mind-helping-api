e5fa95f8790134fc5372e2b5660b0a88e6ee351f
Felipe <feliperocha.dev27@gmail.com>
2025-11-10 19:38:42 -0300

    fix: adicionando o repositorio de agendamento no factoria para cancelar um horario

--- src/factories/hourlies/make-set-cancel-hourly-use-case.ts
+++ src/factories/hourlies/make-set-cancel-hourly-use-case.ts
@@ -1,10 +1,13 @@
 import { PrismaHourlyRepository } from '@/repositories/prisma/prisma-hourly-repository'
+import { PrismaSchedulingRepository } from '@/repositories/prisma/prisma-scheduling-repository'
 import { SetCancelHourlyUseCase } from '@/use-cases/hourlies/set-cancel-hourly-use-case'
 
 export function makeSetCancelHourlyUseCase() {
   const prismaHourlyRepository = new PrismaHourlyRepository()
+  const prismaSchedulingRepository = new PrismaSchedulingRepository()
   const setCancelHourlyUseCase = new SetCancelHourlyUseCase(
-    prismaHourlyRepository
+    prismaHourlyRepository,
+    prismaSchedulingRepository
   )
 
   return setCancelHourlyUseCase
